743dd58a51f05e5ef05f939f9cd8a5109fe276e5
Uwe Riegel <uriegel@hotmail.de>
2020-11-22 16:09:04 +0100

    Adapted to FirePlayer

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,21 @@ import * as fs from "fs/promises"
 
 const PORT = process.env.PORT || 9865
 const PATH = process.env.MEDIA_PATH || '/media/'
+const RELATIVE_URL = process.env.RELATIVE_URL || '/media'
 
 const router = express.Router()
 
-router.get('/video/list', async (req, res) => {
+router.get(`${RELATIVE_URL}/list`, async (req, res) => {
     const files = await fs.readdir(PATH)  
     res.send(JSON.stringify({ files }))
 })
 
-router.get('/video/*', async (req, res) => {
+router.get(`${RELATIVE_URL}/*`, async (req, res) => {
     const url = req.url.substr(7)
     const file = `${PATH}/${decodeURI(url)}`.replace(/\+/gi, " ")
     try {
         const filePath = fsAll.existsSync(file + ".mp4") ? file + ".mp4" : file + ".mkv"
+        const contentType = filePath.endsWith(".mkv") ? 'video/mkv' : 'video/mp4'
         const stat = await fs.stat(filePath)
         
         const fileSize = stat.size
@@ -35,7 +37,7 @@ router.get('/video/*', async (req, res) => {
                 'Content-Range': `bytes ${start}-${end}/${fileSize}`,
                 'Accept-Ranges': 'bytes',
                 'Content-Length': chunksize,
-                'Content-Type': 'video/mkv',
+                'Content-Type': contentType,
             }
             
             res.writeHead(206, head);
@@ -43,7 +45,7 @@ router.get('/video/*', async (req, res) => {
         } else {
             const head = {
                 'Content-Length': fileSize,
-                'Content-Type': 'video/mkv',
+                'Content-Type': contentType,
             }
     
             res.writeHead(200, head);
