05fe2a03b5f3b790aaee1c0b53d6242945452c12
uriegel <uriegel@hotmail.de>
2021-06-20 11:14:53 +0200

    mp4 and mkv

--- src/requests.rs
+++ src/requests.rs
@@ -5,6 +5,12 @@ use std::{fs, path::Path};
 use warp::Reply;
 use warp_range::get_range;
 
+struct VideoFile {
+    path: String,
+    media_type: String
+
+}
+
 #[derive(Serialize)]
 pub struct VideoList {
     files: Vec<String>
@@ -38,17 +44,41 @@ pub async fn get_video_list(path: String)->Result<VideoList, warp::Rejection> {
     Ok(VideoList{ files })
 }
 
-// TODO: Content_type
 pub async fn get_video(file: String, path: String) -> Result<impl warp::Reply, warp::Rejection> {
-    get_range("".to_string(), &combine_path(path, file), "video/mp4").await
+    get_video_range_impl(file, path, "".to_string()).await
 }
 
 pub async fn get_video_range(file: String, path: String, range_header: String) -> Result<impl warp::Reply, warp::Rejection> {
-    get_range(range_header, &combine_path(path, file), "video/mp4").await
+    get_video_range_impl(file, path, range_header).await
 }
 
-fn combine_path(path: String, file: String)->String {
-    let file = percent_encoding::percent_decode(file.as_bytes()).decode_utf8().unwrap();
-    Path::new(&path).join(file.to_string()).to_str().unwrap().to_string()
+async fn get_video_range_impl(file: String, path: String, range_header: String) -> Result<impl warp::Reply, warp::Rejection> {
+    let video = get_video_file(&path, file);
+    get_range(range_header, &video.path, &video.media_type).await
 }
+
+fn get_video_file(path: &str, file: String)->VideoFile {
+    fn combine_path(path: &str, file: String, ext: &str)->Option<String> {
+        let file_with_ext = file + ext;
+        println!("Hä {} {}", path, file_with_ext);
+        let file = percent_encoding::percent_decode(file_with_ext.as_bytes()).decode_utf8().unwrap();
+        let path = Path::new(&path).join(file.to_string());
+
+        println!("Feile {}", file);
+
+        if path.exists() { Some(path.to_string_lossy().to_string())} else { None }
+    }
+    
+    if let Some(mp4) = combine_path(path, file.clone(),&".mp4") {
+        VideoFile{ path: mp4, media_type: "video/mp4".to_string() }
+    } else {
+        if let Some(mkv) = combine_path(path, file, &".mkv") {
+            VideoFile{ path: mkv, media_type: "video/mkv".to_string() }
+        } else {
+            // TODO 
+            panic!("not mp4 and not mkv")
+        }
+    }
+}
+
 
