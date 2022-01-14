a8cdf54efb0ccf6be3bb2ab8852ff7d8d99509be
Uwe Riegel <uriegel@hotmail.de>
2022-01-14 21:48:55 +0100

    http

--- src/main.rs
+++ src/main.rs
@@ -77,14 +77,14 @@ async fn main() {
         route_acme
         .or(route_static);
 
-    // warp::serve(routes)
-    //     .run(([0, 0, 0, 0], port))
-    //     .await; 
-
     warp::serve(routes)
-        .tls()
-        .cert_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_crt_familie_uriegel_de.crt")
-        .key_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_key_familie_uriegel_de.key")
-        .run(([0, 0, 0, 0], tls_port))
+        .run(([0, 0, 0, 0], port))
         .await; 
+
+    // warp::serve(routes)
+    //     .tls()
+    //     .cert_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_crt_familie_uriegel_de.crt")
+    //     .key_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_key_familie_uriegel_de.key")
+    //     .run(([0, 0, 0, 0], tls_port))
+    //     .await; 
 }
