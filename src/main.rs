4b1400edc9e9221d8f92013fb5fa9b3f9fd1624d
Uwe Riegel <uriegel@hotmail.de>
2022-01-14 21:31:28 +0100

    common name changed

--- src/main.rs
+++ src/main.rs
@@ -83,8 +83,8 @@ async fn main() {
 
     warp::serve(routes)
         .tls()
-        .cert_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_crt_uriegel_de.crt")
-        .key_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_key_uriegel_de.key")
+        .cert_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_crt_familie_uriegel_de.crt")
+        .key_path("/home/uwe/Projekte/UwebServerCert/cert/3484110687051588624_key_familie_uriegel_de.key")
         .run(([0, 0, 0, 0], tls_port))
         .await; 
 }
