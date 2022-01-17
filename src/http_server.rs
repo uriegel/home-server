1930c9fcb469b988ae0639f2921584af83355cfa
Uwe Riegel <uriegel@hotmail.de>
2022-01-17 07:26:54 +0100

    proxy_request to fritz

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,5 +1,7 @@
+use hyper::{header::HOST, HeaderMap};
 use tokio::runtime::Runtime;
 use warp::{serve, path::{tail, Tail}, Filter, fs::dir};
+use warp_reverse_proxy::{reverse_proxy_filter, proxy_to_and_forward_response, extract_request_data_filter};
 
 use crate::warp_utils::{simple_file_send, add_headers};
 
@@ -18,11 +20,23 @@ pub fn start_http_server(rt: &Runtime) {
         dir("webroot")
         .map(add_headers);
 
+    let request_filter = extract_request_data_filter();
+    let fritz_proxy = warp::any()
+        .map(|| ("http://fritz.box/".to_string(), "".to_string()))
+        .untuple_one()
+        .and(request_filter)
+        .and_then(|pa, bp, uri, ps, m, headers: HeaderMap, body| {
+            let mut proxy_headers = headers.clone();
+            proxy_headers.remove(HOST);
+            proxy_headers.insert(HOST, "fritz.box".parse().unwrap());
+            proxy_to_and_forward_response(pa, bp, uri, ps, m, proxy_headers, body)
+        });
+
     let port = 8080;
 
     let routes = 
         route_acme
-        .or(route_static);    
+        .or(fritz_proxy);    
 
     rt.spawn(async move {
         serve(routes)
