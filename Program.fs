4bdeedac4ac10ab65ddc1a8ca5ef002962fa2ecb
Uwe Riegel <uriegel@hotmail.de>
2022-02-19 11:12:37 +0100

    Configurations, secure and insecure ports

--- Program.fs
+++ Program.fs
@@ -2,6 +2,7 @@ open Giraffe
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Server.Kestrel.Core
+open Microsoft.AspNetCore.Server.Kestrel.Https
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 
@@ -14,9 +15,12 @@ let webApp =
 
 let configureApp (app : IApplicationBuilder) = app.UseGiraffe webApp
 let configureServices (services : IServiceCollection) = services.AddGiraffe() |> ignore
+let httpsOptions (options: HttpsConnectionAdapterOptions) = ()
+let httpsListenOptions (options: ListenOptions) = options.UseHttps(httpsOptions)|> ignore
 let configureKestrel (options: KestrelServerOptions) = 
     options.ListenAnyIP(8080)
-    options.ListenAnyIP(9865)
+    options.ListenAnyIP(4433, httpsListenOptions)
+
 let webHostBuilder (webHostBuilder: IWebHostBuilder) = 
     webHostBuilder
         .ConfigureKestrel(configureKestrel)
