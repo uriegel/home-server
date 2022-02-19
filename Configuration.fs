670e091dba766be7171fa96e6e3ca4dfbd1dd2e5
Uwe Riegel <uriegel@hotmail.de>
2022-02-19 19:33:49 +0100

    get certificate railroad

--- Configuration.fs
+++ Configuration.fs
@@ -5,12 +5,29 @@ open Microsoft.AspNetCore.Server.Kestrel.Core
 open Microsoft.AspNetCore.Server.Kestrel.Https
 open Utils.OptionFish
 
-let getPortFromEnvironment = Utils.getEnvironmentVariable >=> Utils.parseInt 
+let OptionFrom2Options a b = 
+    match (a, b) with
+    | (Some a, Some b) -> Some (a, b)
+    | _ -> None
 
+let getPortFromEnvironment = Utils.getEnvironmentVariable >=> Utils.parseInt 
 let httpPort () = getPortFromEnvironment "SERVER_PORT" |> Option.defaultValue 80
 let httpsPort () = getPortFromEnvironment "SERVER_TLS_PORT" |> Option.defaultValue 443
 
-let httpsOptions (options: HttpsConnectionAdapterOptions) = ()
+let getLetsEncryptDirectory () = Utils.getEnvironmentVariable "LETS_ENCRYPT_DIR"
+let appendCertFile certFile = Utils.pathCombine certFile
+
+let makeCertFileName certFile = getLetsEncryptDirectory >=> Utils.pathCombine certFile // |> Option.defaultValue ""
+let makeCertificatePath = makeCertFileName "cert.pem" //|> Option.defaultValue ""
+let makeKeyPath = makeCertFileName "key.pem"
+let getCertificate () = makeCertificatePath ()
+let getKey () = makeKeyPath () 
+let getCertValuePair () = OptionFrom2Options (getCertificate ()) (getKey ())
+let getCertificateFromFile = getCertValuePair >=> fun (a, b) -> Utils.getCertificateFromFile a b
+
+let httpsOptions (options: HttpsConnectionAdapterOptions) = 
+    options.ServerCertificate <- getCertificateFromFile () |> Option.defaultValue null
+
 let httpsListenOptions (options: ListenOptions) = options.UseHttps(httpsOptions)|> ignore
 let configureKestrel (options: KestrelServerOptions) = 
     options.ListenAnyIP(httpPort ())
--- Utils.fs
+++ Utils.fs
@@ -1,5 +1,7 @@
 module Utils
 
+open System.Security.Cryptography.X509Certificates
+
 module OptionFish = 
     let (>=>) switch1 switch2 x =
         match switch1 x with
@@ -23,6 +25,12 @@ let parseInt (str: string) =
 
 let getEnvironmentVariable key =
     exceptionToOption (fun () -> System.Environment.GetEnvironmentVariable key)
+
+let getCertificateFromFile certPath keyPath =
+    exceptionToOption (fun () -> X509Certificate2.CreateFromPemFile (certPath, keyPath))
+
+let pathCombine subPath path =
+    exceptionToOption (fun () -> System.IO.Path.Combine (path, subPath))
 
 // let httpsPort () = Utils.getEnvironmentVariable "SERVER_PORT"
 // let httpsPort2 () = Utils.getEnvironmentVariable "SERVER_PORTchen"
