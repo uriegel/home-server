69cc190bcb3d141fdb36cef74384e541b2f70516
Uwe Riegel <uriegel@hotmail.de>
2022-02-20 16:18:41 +0100

    hide internal functions

--- Configuration.fs
+++ Configuration.fs
@@ -7,25 +7,25 @@ open Microsoft.AspNetCore.Server.Kestrel.Https
 open Utils
 open Utils.OptionFish
 
-let getPortFromEnvironment = getEnvironmentVariable >=> parseInt 
-let httpPort () = getPortFromEnvironment "SERVER_PORT" |> Option.defaultValue 80
-let httpsPort () = getPortFromEnvironment "SERVER_TLS_PORT" |> Option.defaultValue 443
-
-let getLetsEncryptDirectory () = getEnvironmentVariable "LETS_ENCRYPT_DIR"
-let makeCertFileName certFile = getLetsEncryptDirectory >=> pathCombine certFile 
-let makeCertificatePath = makeCertFileName "cert.pem" 
-let makeKeyPath = makeCertFileName "key.pem"
-let getCertificate () = makeCertificatePath ()
-let getKey () = makeKeyPath () 
-let getCertValuePair () = OptionFrom2Options (getCertificate ()) (getKey ())
-let getCertificateFromFile = getCertValuePair >=> fun (a, b) -> getCertificateFromFile a b
-
 let getIntranetHost () = getEnvironmentVariable "INTRANET_HOST"
 
-let httpsOptions (options: HttpsConnectionAdapterOptions) = 
-    options.ServerCertificate <- getCertificateFromFile () |> Option.defaultValue null
-
-let httpsListenOptions (options: ListenOptions) = options.UseHttps(httpsOptions)|> ignore
 let configureKestrel (options: KestrelServerOptions) = 
+    let getCertificateFromFile = 
+        let makeCertFileName certFile = 
+            let getLetsEncryptDirectory () = getEnvironmentVariable "LETS_ENCRYPT_DIR"
+            getLetsEncryptDirectory >=> pathCombine certFile 
+        let makeCertificatePath = makeCertFileName "cert.pem" 
+        let makeKeyPath = makeCertFileName "key.pem"
+        let getCertificate () = makeCertificatePath ()
+        let getKey () = makeKeyPath () 
+        let getCertValuePair () = OptionFrom2Options (getCertificate ()) (getKey ())
+        getCertValuePair >=> fun (a, b) -> getCertificateFromFile a b
+    let httpsOptions (options: HttpsConnectionAdapterOptions) = 
+        options.ServerCertificate <- getCertificateFromFile () |> Option.defaultValue null
+    let httpsListenOptions (options: ListenOptions) = options.UseHttps(httpsOptions)|> ignore
+    let getPortFromEnvironment = getEnvironmentVariable >=> parseInt 
+    let httpPort () = getPortFromEnvironment "SERVER_PORT" |> Option.defaultValue 80
+    let httpsPort () = getPortFromEnvironment "SERVER_TLS_PORT" |> Option.defaultValue 443
+
     options.ListenAnyIP(httpPort ())
     options.ListenAnyIP(httpsPort (), httpsListenOptions)
--- Routes.fs
+++ Routes.fs
@@ -8,34 +8,35 @@ open System.Threading.Tasks
 open Configuration
 open Requests
 
-let skip : HttpFuncResult = Task.FromResult None
+let configureRoutes (app : IApplicationBuilder) = 
+    let skip : HttpFuncResult = Task.FromResult None
 
-let host (host: string) (next: HttpFunc) (ctx: HttpContext) =
-    match ctx.Request.Host.Host with
-    | value when value = host -> next ctx
-    | _ -> skip
+    let host (host: string) (next: HttpFunc) (ctx: HttpContext) =
+        match ctx.Request.Host.Host with
+        | value when value = host -> next ctx
+        | _ -> skip
 
-let insecureHost host (next: HttpFunc) (ctx: HttpContext) =
-    match ctx.Request.Host.Host, ctx.Request.IsHttps with
-    | value, false when value = host -> next ctx
-    | _ -> skip
+    let insecureHost host (next: HttpFunc) (ctx: HttpContext) =
+        match ctx.Request.Host.Host, ctx.Request.IsHttps with
+        | value, false when value = host -> next ctx
+        | _ -> skip
 
-let secureHost host (next: HttpFunc) (ctx: HttpContext) =
-    match ctx.Request.Host.Host, ctx.Request.IsHttps with
-    | value, true when value = host -> next ctx
-    | _ -> skip
+    let secureHost host (next: HttpFunc) (ctx: HttpContext) =
+        match ctx.Request.Host.Host, ctx.Request.IsHttps with
+        | value, true when value = host -> next ctx
+        | _ -> skip
 
-let allHosts (next: HttpFunc) (ctx: HttpContext) =
-    next ctx
-
-let routes =
-    choose [
-        host <| (getIntranetHost () |> Option.defaultValue "") >=>
-            choose [
-                route "/ping" >=> show ()
-                route "/"     >=> htmlFile "webroot/index.html" ]
-        secureHost "fritz.uriegel.de" >=> text "Zur Fritzbox"
-        allHosts >=> text "Falscher Host"
-    ]
-
-let configureRoutes (app : IApplicationBuilder) = app.UseGiraffe routes    
+    let allHosts (next: HttpFunc) (ctx: HttpContext) =
+        next ctx
+    
+    let routes =
+        choose [
+            host <| (getIntranetHost () |> Option.defaultValue "") >=>
+                choose [
+                    route "/ping" >=> show ()
+                    route "/"     >=> htmlFile "webroot/index.html" ]
+            secureHost "fritz.uriegel.de" >=> text "Zur Fritzbox"
+            allHosts >=> text "Falscher Host"
+        ]
+    
+    app.UseGiraffe routes    
