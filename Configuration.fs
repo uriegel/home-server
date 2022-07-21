4043fd20405e35243d3d02a2842a980a191ca290
Uwe Riegel <uriegel@hotmail.de>
2022-07-21 14:34:23 +0200

    Update certificate

--- Configuration.fs
+++ Configuration.fs
@@ -23,9 +23,13 @@ let configureKestrel (options: KestrelServerOptions) =
             getLetsEncryptPath >> Option.map combineWithCertFile
         let getCertificate (file: string) = Some(new System.Security.Cryptography.X509Certificates.X509Certificate2(file, "uriegel"))
         makeCertFileName "certificate.pfx" >=> getCertificate
+
+    let getCertificate () = 
+        // Memoize this call, reset it every day
+        getCertificateFromFile () |> Option.defaultValue null
         
     let httpsOptions (options: HttpsConnectionAdapterOptions) = 
-        options.ServerCertificate <- getCertificateFromFile () |> Option.defaultValue null
+        options.ServerCertificateSelector <- fun a b -> getCertificate ()
     let httpsListenOptions (options: ListenOptions) = 
         options.Protocols <- HttpProtocols.Http1AndHttp2AndHttp3    
         options.UseHttps(httpsOptions) |> ignore
@@ -41,20 +45,3 @@ let configureKestrel (options: KestrelServerOptions) =
 
 
 
-// TODO reload Certificate
-// There is a possibility to reload the certificate without restarting. basically there is a callback mechanism which loads the certificate for each request.
-
-// .UseKestrel(options =>
-//  {
-//    options.ConfigureHttpsDefaults(o =>
-//    {
-//        o.ServerCertificateSelector = (context, dnsName) =>
-//        {
-//           return GetCertificateFromPath();
-//        };
-//     });
-//  });
-
-// since it calls this GetCertificateFromPath method for each request so you have to cache the certificate somehow inside the GetCertificateFromPath() method and only read when it is changed.
-
-// it should be possible with some way by checking modified date or something.
