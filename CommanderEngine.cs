31ea4e39edc24973603b7671c6618ead796e91e2
Uwe Riegel <uriegel@hotmail.de>
2023-08-17 07:57:36 +0200

    GetFiles

--- CommanderEngine.cs
+++ CommanderEngine.cs
@@ -1,11 +1,28 @@
+using LinqTools;
+
 static class CommanderEngine
 {
-    public static async Task<RemoteItem[]> GetFiles(Input input)
-        => new RemoteItem[] 
-            {
-                new("Hallo", 9, false, false, 1231),
-                new("Hallo 5", 9, false, false, 4567)
-            };
+    public static Task<RemoteItem[]> GetFiles(Input input)
+        => input.path.With(
+                p => new DirectoryInfo(p),
+                i => i
+                        .GetDirectories()
+                        .Select(d => new RemoteItem(
+                            d.Name,
+                            0,
+                            true,
+                            (i.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden || d.Name.StartsWith('.'),
+                            (new DateTimeOffset(d.LastWriteTime).ToUnixTimeMilliseconds())))
+                    .Concat(i
+                        .GetFiles()
+                        .Select(f => new RemoteItem(
+                            f.Name,
+                            f.Length,
+                            false,
+                            (f.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden || f.Name.StartsWith('.'),
+                            (new DateTimeOffset(f.LastWriteTime).ToUnixTimeMilliseconds()))))
+                    .ToArray()
+                    .ToAsync());
 
     public record Input(string path);
     public record RemoteItem(
