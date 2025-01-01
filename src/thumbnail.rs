d5ef2f2bd9020e10fcd2ee151dfa5479e29f8eb6
uriegel <uriegel@hotmail.de>
2025-01-01 10:58:06 +0100

    image thumbnail

--- src/thumbnail.rs
+++ src/thumbnail.rs
@@ -1,5 +1,23 @@
+use std::io::Cursor;
+use image::{imageops::FilterType, ImageFormat, ImageReader};
+
 pub fn create_thumbnail(image: &str)->Option<Vec<u8>> {
-    None
+    ImageReader::open(image)
+    .ok()
+    .and_then(|ir|ir.decode().ok())
+    .map(|i|i.resize(64, 64, FilterType::Lanczos3))
+    .and_then(|r|{
+        let mut bytes: Vec<u8> = Vec::new();
+        let res = r.write_to(&mut Cursor::new(&mut bytes), ImageFormat::Jpeg).ok();
+        if res.is_some() {
+            Some(bytes)
+        } else {
+            None
+        }
+    })
+
+    //None
+
     // let pixbuf = Pixbuf::from_file(image).ok();
     // let new_height = pixbuf.clone().map(|pb|{
     //     let w = pb.width() as f32;
