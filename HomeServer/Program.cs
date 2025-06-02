890e4e74243af4fd75c778912a0f94b892acf59a
uriegel <uriegel@hotmail.de>
2025-06-02 15:21:11 +0200

    Webserver running

--- HomeServer/Program.cs
+++ HomeServer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WebServerLight;
+using WebServerLight.Routing;
+
+using static System.Console;
+
+WriteLine(@"Test site:  http://localhost:5050");
+
+var server =
+    ServerBuilder
+        .New()
+        .Http(5050)
+        // .Route(MethodRoute
+        //         .New(Method.Get)
+        //         .Add(SubpathRoute
+        //                 .New("/image")
+        //                 .Request(GetImage))
+        //         .Add(SubpathRoute
+        //                 .New("/video")
+        //                 .Request(GetVideo)))
+        // .Route(MethodRoute
+        //         .New(Method.Post)
+        //         .Add(SubpathRoute
+        //                 .New("/json/cmd4")
+        //                 .Request(JsonPost4))
+        //         .Add(SubpathRoute
+        //                 .New("/json")
+        //                 .Request(JsonPost)))
+        // .Route(SubpathRoute
+        //         .New("/media")
+        //         .Add(MethodRoute
+        //             .New(Method.Get)
+        //             .Request(GetMediaVideo)))
+
+        .AddAllowedOrigin("http://localhost:5050")
+        .UseRange()
+        .Build();
+    
+server.Start();
+ReadLine();
+server.Stop();
