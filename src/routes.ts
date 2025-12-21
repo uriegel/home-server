87c7ee60680f18a4fa4efbbe469068c21aad77c4
Uwe Riegel <uriegel@hotmail.de>
2025-12-21 17:50:13 +0100

    Serve pictures

--- src/routes.ts
+++ src/routes.ts
@@ -8,11 +8,16 @@ export const router = Router()
 
 const VIDEO_PATH = process.env.VIDEO_PATH || '/video'
 const MUSIC_PATH = process.env.MUSIC_PATH || '/music'
+const PICTURE_PATH = process.env.PICTURE_PATH || '/video'
 console.log("VIDEO_PATH", VIDEO_PATH)
 console.log("MUSIC_PATH", MUSIC_PATH)
+console.log("Picture_PATH", PICTURE_PATH)
 
 router.get('/video{/*splat}', (req, res, n) => serveFile(VIDEO_PATH, req, res, n))
 router.use('/video', serveStatic(VIDEO_PATH))
+
+router.get('/pics{/*splat}', (req, res, n) => serveFile(PICTURE_PATH, req, res, n))
+router.use('/pics', serveStatic(PICTURE_PATH))
 
 router.get('/music{/*splat}', (req, res, n) => serveFile(MUSIC_PATH, req, res, n))
 router.use('/music', serveStatic(MUSIC_PATH))
