fb6f561fa7f3f298b91d16a1399154a817e8c194
Uwe Riegel <uriegel@hotmail.de>
2025-12-24 12:50:45 +0100

    disk management removed

--- src/main.ts
+++ src/main.ts
@@ -22,31 +22,3 @@ function shutdown() {
 }
 
 
-// async function serveAudioTrack(req: express.Request, res: express.Response, track: string) {
-//     await serveRange(req, res, track, "audio/mp3")
-// }
-
-// async function serveAlbums(res: express.Response, directory: string) {
-//     const files = await fs.readdir(directory)  
-//     res.send(JSON.stringify({ files }))
-// }
-
-// async function serveMusic(req: express.Request, res: express.Response) {
-//     try {
-//         const url = req.url.substr(RELATIVE_URL.length + 7)
-//         const file = `${MUSIC_PATH}/${decodeURI(url)}`.replace(/\+/gi, " ")
-//         if (file.toLowerCase().endsWith(".mp3")) 
-//             await serveAudioTrack(req, res, file)
-//         else
-//             await serveAlbums(res, file)
-//     }
-//     catch (err) {
-//         // Handle file not found
-//         if (err !== null && err.code === 'ENOENT') {
-//             res.sendStatus(404);
-//         }
-//     }
-// }
-
-// router.route(`${RELATIVE_URL}/music`).get(serveMusic)
-// router.route(`${RELATIVE_URL}/music/*`).get(serveMusic)
